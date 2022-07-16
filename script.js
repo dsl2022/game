29367670c1411433fc41e38b50fa79b4b402aa19
dsl2055 <job2019tmm@gmail.com>
2022-07-16 14:10:47 -0400

    setup basic grid

--- script.js
+++ script.js
@@ -8,3 +8,35 @@
 // remove image when select delete button
 
 // find some images
+const root = document.querySelector("#root");
+
+const gridContainer = document.createElement("div");
+gridContainer.className = "grid-container";
+
+const rowsArr = [1, 2, 3];
+
+rowsArr.forEach((row) => {
+  const rowContainer = document.createElement("div");
+  rowContainer.className = `row-container-${row}`;
+  rowContainer.style.display = "flex";
+  rowContainer.style.flexDirection = "row";
+  rowContainer.style.width = "900px";
+  rowContainer.style.border = "1px solid black";
+  root.appendChild(rowContainer);
+  for (let i = 1; i < 4; i++) {
+    const column = document.createElement("div");
+    column.id = `${row}-${i}`;
+    column.textContent = `column-${row}-${i}`;
+    column.style.border = "1px solid black";
+    column.style.width = "300px";
+    column.style.height = "300px";
+    rowContainer.appendChild(column);
+    column.addEventListener("click", (e, row, column) =>
+      handleGrid(e, row, column)
+    );
+  }
+});
+
+function handleGrid(e, row, column) {
+  console.log(`hello grid ${e}${row}${column}`);
+}
