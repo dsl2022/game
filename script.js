8a1664352bdb1fb9bc04244972bed5560dd56979
dsl2055 <job2019tmm@gmail.com>
2022-07-16 13:56:04 -0400

    first commit

---
+++ script.js
@@ -0,0 +1,10 @@
+// 1
+// create 9 grids.
+// each grid has an id.
+// each grid has event listner.
+// when click into grid, grid shows an image.
+
+// 2
+// remove image when select delete button
+
+// find some images
